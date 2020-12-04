f18af06d9487bf280abd6f767373cbd0a64a32da
RFagot <48565867+RF-Studio@users.noreply.github.com>
2020-12-04 16:57:20 +0100

    Test mode
    type '1' on UART en sent you back date and temp value AND send it through MRF89

--- MRF89.c
+++ MRF89.c
@@ -6,9 +6,31 @@
  */
 
 #define MRF89_C
-#include "..\Sources\Main.h"
+#include "../Sources/Main.h"
 
 void MRF89_Init(void){
+    char r;
+    MRF89_DEFINE_PORT Config_port;
+
+    Config_port.BIT_CSCON = MRF89_BIT_RB12; // CSCON on RB12
+    Config_port.BIT_CSDATA = MRF89_BIT_RB8; // CSDATA on RB8
+    Config_port.BIT_IRQ0 = MRF89_BIT_RB13; // IRQ0 on RB13
+    Config_port.BIT_IRQ1 = MRF89_BIT_RB10; // IRQ1 on RB10
+    Config_port.BIT_RESET = MRF89_BIT_RB11; // RESET on RB11
     
+
+    Lib_Temp_SPI_Config(1);
+    r = Lib_Temp_MRF89_Config(Config_port);    // config defaut : 93.75 kbits par seconde - P_out = 13 dBm
+    if (r == -1) 
+    {
+        printf("Error config MRF89");
+    }
+    
+    Lib_Temp_MRF89_Ecriture_SPI_FIFO(10,(unsigned char *)"Hello World !");
+    r = Lib_Temp_MRF89_Attend_Fin_Transmission_Hertzienne();
+    if(r == -1){
+        printf("Erreur de transmition");
+    }
+    Lib_Temp_MRF89_Change_Mode(MRF89_MODE_SLEEP);
 }
 
--- SPI.c
+++ SPI.c
@@ -8,6 +8,40 @@
 #define SPI_C
 #include "../Sources/Main.h"
 
-void SPI_Init(void){
+//------------------------------------------------------------------------------
+// Interruption SPI TX
+//------------------------------------------------------------------------------
+void __attribute__((interrupt, auto_psv)) _SPI1TXInterrupt(void)
+{
+    
+    IFS0bits.SPI1TXIF       = 0;            // Init du bit de reception
+}
+//------------------------------------------------------------------------------
+
+
+
+
+//------------------------------------------------------------------------------
+// Interruption SPI RX
+//------------------------------------------------------------------------------
+void __attribute__((interrupt, auto_psv)) _SPI1RXInterrupt(void)
+{
+    
+    IFS3bits.SPI1RXIF       = 0;            // Init du bit de reception
+}
+//------------------------------------------------------------------------------
+
+
+
+void SPI_Init(void){ 
+    
+    Lib_Temp_SPI_Config(1); // on choisis le SPI num 1
+    
+    Lib_Temp_REMAP_SIGNAL_OUT_SUR_RPx(REMAP_SIGNAL_OUT_SCK1OUT, RP14_RB14_BORNE_25); // Remappage du signal sur SCK1 sur la borne 25
+    Lib_Temp_REMAP_SIGNAL_OUT_SUR_RPx(REMAP_SIGNAL_OUT_SDO1, RP15_RB15_BORNE_26); // Remappage du signal sur SD01 sur la borne 26
+    Lib_Temp_REMAP_SIGNAL_IN_SUR_RPx(REMAP_SIGNAL_IN_SDI1, RP9_RB9_BORNE_18); // Remappage du signal sur SDI1 sur la borne 18
+    
+    
     
 }
+
